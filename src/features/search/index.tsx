f66f1e6aed88d58fe4e6d446afb79854bbc98942
Siddhant Prasad <siddhantprasad.tvnl@gmail.com>
2025-05-01 21:54:04 +0530

    Add search filter in Home page

--- src/features/search/index.tsx
+++ src/features/search/index.tsx
@@ -5,21 +5,37 @@ import LocationInput from './location-input';
 import DateSelectInput from './date-select-input';
 import OccupancyInput from './occupancy-input';
 import { Button } from '@/components/ui/button';
+import dayjs from 'dayjs';
 
 const Search = () => {
-
-  const form = useForm();
-
-  function onSubmit(data: any) {
-    console.log(data);    
-  }
+  
+  const form = useForm({
+     defaultValues: {
+       city: '',
+       roomsCount: 1,
+       bookingDates: {
+         from: dayjs().toDate(),
+         to: dayjs().add(1, 'day').toDate()
+       }
+     },
+   });
+ 
+   function onSubmit(data) {
+     const sendData = {
+       city: data.city,
+       roomsCount: data.roomsCount,
+       startDate: dayjs(data.bookingDates.from).format('YYYY-MM-DD'),
+       endDate: dayjs(data.bookingDates.to).format('YYYY-MM-DD')
+     };
+     console.log('Search data', sendData);
+   }
 
   return (
     <section className='container'>
       <Form {...form}>
         <form 
         onSubmit={form.handleSubmit(onSubmit)} 
-        className='flex flex-col gap-1 p-1 bg-yellow-500 lg:flex-row lg:items-center rounded'
+        className='flex flex-col gap-1 p-1 bg-yellow-500 lg:flex-row lg:items-center rounded h-16'
         >
           <LocationInput form={form} />
           <DateSelectInput form={form} />
